06b24b4e8a0c236bf901a6bdb4d92761e1c1a0c8
Georgi <georgi_yosifov99@abv.bg>
2021-03-05 12:14:48 +0200

    Add server unit tests

---
+++ Quiz.Tests/IdentityServiceTests.cs
@@ -0,0 +1,65 @@
+﻿using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using Quiz.Server.Data;
+using Quiz.Server.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Quiz.Tests
+{
+    public class IdentityServiceTests
+    {
+        [Fact]
+        public async Task LoginAsync_Should_ReturnCorrectData()
+        {
+            var options = new DbContextOptionsBuilder<DataContext>()
+                  .UseInMemoryDatabase(databaseName: "quiz")
+                  .Options;
+
+            using (var context = new DataContext(options))
+            {
+                await CleanUpUsers(context);
+
+                const string username = "test";
+                var service = new IdentityService(context);
+                var response = await service.LoginAsync(username);
+
+                response.Username.Should().Be(username);
+                var actual = await context.Users.CountAsync();
+                actual.Should().Be(1);
+            }
+        }
+
+        private static async Task CleanUpUsers(DataContext context)
+        {
+            var users = await context.Users.ToListAsync();
+            context.Users.RemoveRange(users);
+        }
+
+        [Fact]
+        public async Task LogoutAsync_Should_SetIsLoggedInToFalse()
+        {
+            var options = new DbContextOptionsBuilder<DataContext>()
+                .UseInMemoryDatabase(databaseName: "quiz")
+                .Options;
+
+            using (var context = new DataContext(options))
+            {
+                const string username = "test";
+                var service = new IdentityService(context);
+                var response = await service.LoginAsync(username);
+
+                response.Username.Should().Be(username);
+
+                await service.LogoutAsync(response.Id);
+
+                var actual = await context.Users.FirstOrDefaultAsync(x => x.Id == response.Id);
+                actual.IsLoggedIn.Should().BeFalse();
+            }
+        }
+    }
+}
---
+++ Quiz.Tests/QuizServiceTests.cs
@@ -0,0 +1,65 @@
+﻿using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Quiz.Server.Data;
+using Quiz.Server.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Quiz.Tests
+{
+    public class QuizServiceTests
+    {
+        [Fact]
+        public async Task GetCategories_Should_ReturnAllCategories()
+        {
+            var options = new DbContextOptionsBuilder<DataContext>()
+                 .UseInMemoryDatabase(databaseName: "quiz")
+                 .Options;
+
+            using (var context = new DataContext(options))
+            {
+                var service = new QuizService(context);
+
+                context.Categories.Add(new Category() { Id = 1, Name = "TestCategory", Questions = new List<Question>() });
+                context.Categories.Add(new Category() { Id = 2, Name = "TestCategory2", Questions = new List<Question>() });
+                context.SaveChanges();
+
+                var categories = service.GetCategories();
+
+                categories.Count.Should().Be(2);
+                categories[0].Name.Should().Be("TestCategory");
+            }
+        }
+
+        [Fact]
+        public async Task CreateAsync_Should_ReturnCorrectQuiz()
+        {
+            var options = new DbContextOptionsBuilder<DataContext>()
+                            .UseInMemoryDatabase(databaseName: "quiz")
+                            .Options;
+
+            using (var context = new DataContext(options))
+            {
+                const string userId = "test";
+                var service = new QuizService(context);
+
+                context.Questions.Add(new Question() { Id = 1 });
+                context.Questions.Add(new Question() { Id = 2 });
+                context.Questions.Add(new Question() { Id = 3 });
+                context.Questions.Add(new Question() { Id = 4 });
+                context.Questions.Add(new Question() { Id = 5 });
+                context.SaveChanges();
+
+                var quizId = await service.CreateAsync(userId);
+                quizId.Should().Be(1);
+
+                var questionQuizzes = context.QuestionQuizzes.Where(x => x.QuizId == quizId).ToList();
+                questionQuizzes.Count.Should().Be(5);
+            }
+        }
+    }
+}
